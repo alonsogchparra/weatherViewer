b2fb3d87b6d2baca3a133bdc9d596f38b9b05f22
Alonsop <alonsop@uakami.com>
2018-04-06 21:12:38 -0400

    Adding index file on action folder with axios to get access to the openweather api

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'e70ef77c543795c52794dd15019e2312';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export const fetchWeather = (city) => {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
