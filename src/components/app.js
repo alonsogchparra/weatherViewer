e7f0c919763c14269a52e4290bcfb349bd6f4a07
Alonsop <alonsop@uakami.com>
2018-04-06 21:50:15 -0400

    container added on App component

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import SearchBar from '../containers/search_bar'; 
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        Hello!!
+        <SearchBar />
       </div>
     )
   }
